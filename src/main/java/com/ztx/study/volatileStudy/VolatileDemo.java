82c178d56cbfe29036787fce261b8073916c0a96
LAPTOP-UKLJH07L\ztx <1390958241@qq.com>
2020-03-16 23:09:31 +0800

    提交哈

--- src/main/java/com/ztx/study/volatileStudy/VolatileDemo.java
+++ src/main/java/com/ztx/study/volatileStudy/VolatileDemo.java
@@ -1,5 +1,35 @@
 package com.ztx.study.volatileStudy;
 
+class MyWork{
+	volatile int number = 20;
+	
+	public void changeNum(){
+		this.number = 60;
+	}
+}
+
 public class VolatileDemo {
 
+	public static void main(String args[]){
+		MyWork work = new MyWork();
+		
+		Thread thread = new Thread("AAA"){
+			public void run(){
+				System.out.println(Thread.currentThread().getName() + " start.");
+				try {
+					Thread.sleep(3000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				work.changeNum();
+				System.out.println(Thread.currentThread().getName() + " value change.");
+			}
+		};
+		thread.start();
+		
+		while(work.number == 20){
+			
+		}
+		System.out.println(Thread.currentThread().getName() + " change success, value:" + work.number);
+	}
 }
